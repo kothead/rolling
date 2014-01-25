cd0b5cb6493408e151dc12f127e13c08e7790bea
kothead <gdmmtf@gmail.com>
2014-01-25 16:33:42 +0600

    random generation

--- src/com/codepenguins/rolling/model/GameScene.java
+++ src/com/codepenguins/rolling/model/GameScene.java
@@ -1,15 +1,35 @@
 package com.codepenguins.rolling.model;
 
+import java.util.Random;
+
+import com.codepenguins.rolling.Game;
+
 public class GameScene extends Scene {
 
 	private static final float CLOUD_PROBABILITY = 0.5f;
 	private static final float PLANE_PROBABILITY = 0.1f;
+	private static final float SCENE_MULTIPLIER = 2;
 	
 	private Player player;
 
 	public GameScene() {
 		player = new Player();
 		addGameObject(player);
+	}
+	
+	@Override
+	public void processScene(long tick) {
+		super.processScene(tick);
+		
+		double cloudProb = Math.random();
+		if (cloudProb < CLOUD_PROBABILITY / Game.TARGET_FPS) {
+			generateCloud();
+		}
+		
+		double planeProb = Math.random();
+		if (cloudProb < PLANE_PROBABILITY / Game.TARGET_FPS) {
+			generatePlane();
+		}
 	}
 	
 	public void generateCloud() {

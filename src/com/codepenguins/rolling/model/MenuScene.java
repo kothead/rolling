b7b33a4305f4068602dd63f91df46fd6ded95dd5
U-jhaampi\kettricken <kettricken@jhaampi.(none)>
2014-01-25 12:47:07 +0400

    added textobjects

---
+++ src/com/codepenguins/rolling/model/MenuScene.java
@@ -0,0 +1,11 @@
+package com.codepenguins.rolling.model;
+
+public class MenuScene extends Scene {
+
+	@Override
+	public void processScene(long tick) {
+		super.processScene(tick);
+		
+	}
+	
+}
---
+++ src/com/codepenguins/rolling/model/TextObject.java
@@ -0,0 +1,30 @@
+package com.codepenguins.rolling.model;
+
+public class TextObject {
+	
+	private int x;
+	private int y;
+	private String text;
+	private int fontId;
+	private int color;
+	
+	public int getX() {
+		 return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+	public String getText() {
+		return text;
+	}
+	
+	public int getFontId() {
+		return fontId;
+	}
+	
+	public int getColor() {
+		return color;
+	}
+}

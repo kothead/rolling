0704f1cfc82d00c83ee58fd9493266d4f1e75358
kothead <gdmmtf@gmail.com>
2014-01-25 15:39:22 +0600

    deleted logs

--- src/com/codepenguins/rolling/model/GameScene.java
+++ src/com/codepenguins/rolling/model/GameScene.java
@@ -2,7 +2,10 @@ package com.codepenguins.rolling.model;
 
 public class GameScene extends Scene {
 
+	private Player player;
+
 	public GameScene() {
-		
+		player = new Player();
+		addGameObject(player);
 	}
 }
